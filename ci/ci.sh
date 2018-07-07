0866abb256a0780b20a18ddb6cb694c15823e2df
agravgaard <andreasga22@gmail.com>
2018-07-08 00:32:40 +0200

    Third attempt at gitlab CI! (lol i had .sh in gitignore all this time)

---
+++ ci/ci.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+set -xe
+
+if [[ "$CI_RUNNER_TAGS" = *"agravgaard-runner"* ]]; then
+    MAKE_CONCURRENCY="-j13"
+else
+    MAKE_CONCURRENCY="-j1"
+fi;
+
+mkdir build && cd build
+
+echo Test building: $BUILD_TESTING
+
+if [ "$BUILD_TESTING" == "YES" ]; then
+    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON
+    cmake .. 
+    make $MAKE_CONCURRENCY
+else
+    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=OFF
+    cmake .. 
+    make $MAKE_CONCURRENCY
+fi
