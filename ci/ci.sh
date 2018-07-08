255deaea80a86a4f52cd9117bcb2296f0a5a86ce
agravgaard <andreasga22@gmail.com>
2018-07-08 13:16:19 +0200

    Third.3 attempt at gitlab CI! (Only make CbctRecon+dependencies)

--- ci/ci.sh
+++ ci/ci.sh
@@ -4,7 +4,7 @@ set -xe
 if [[ "$CI_RUNNER_TAGS" = *"agravgaard-runner"* ]]; then
     MAKE_CONCURRENCY="-j13"
 else
-    MAKE_CONCURRENCY="-j3"
+    MAKE_CONCURRENCY="-j2"
 fi;
 
 mkdir build && cd build
@@ -14,9 +14,9 @@ echo Test building: $BUILD_TESTING
 if [ "$BUILD_TESTING" == "YES" ]; then
     cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON
     cmake .. 
-    make $MAKE_CONCURRENCY
+    make $MAKE_CONCURRENCY CbctRecon
 else
     cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=OFF
     cmake .. 
-    make $MAKE_CONCURRENCY
+    make $MAKE_CONCURRENCY CbctRecon
 fi
