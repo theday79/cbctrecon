e0f41549e8ceebc04dd76e2c117aeacf32eec28d
agravgaard <andreasga22@gmail.com>
2018-09-02 12:23:02 +0200

    Build test (and tinyrefl) on gitlab-ci

--- ci/ci.sh
+++ ci/ci.sh
@@ -12,11 +12,11 @@ mkdir build && cd build
 echo Test building: $BUILD_TESTING
 
 if [ "$BUILD_TESTING" == "YES" ]; then
-    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON
+    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON
     cmake .. 
-    make $MAKE_CONCURRENCY CbctRecon
+    make $MAKE_CONCURRENCY CbctRecon CbctRecon_test
 else
-    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=OFF
+    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=OFF -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON
     cmake .. 
-    make $MAKE_CONCURRENCY CbctRecon
+    make $MAKE_CONCURRENCY CbctRecon CbctRecon_test
 fi
