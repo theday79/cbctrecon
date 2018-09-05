ea95c11150616deb91486824778f9aab15788e29
agravgaard <andreasga22@gmail.com>
2018-09-05 14:56:21 +0200

    GitLabCI: Docker image fixes

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -3,10 +3,12 @@
 REPO="andreasga"
 NAME="cbctrecon"
 
-docker build --tag ${REPO}/${NAME}:gcc7-x86_64 docker/gcc-7-x86_64
-docker push ${REPO}/${NAME}:gcc7-x86_64
+CC="gcc-7-x86_64"
+docker build --tag ${REPO}/${NAME}:${CC} docker/${CC}
+docker push ${REPO}/${NAME}:${CC}
 
-docker build --tag ${REPO}/${NAME}:gcc7-x86_64-DCMTK3.6.3-ITKv4.13.1-RTKmaster docker/gcc-7-x86_64-DCMTK3.6.3-ITKv4.13.1-RTKmaster 
-docker push ${REPO}/${NAME}:gcc7-x86_64-DCMTK3.6.3-ITKv4.13.1-RTKmaster 
+DCMIRTK="DCMTK363-ITKv4131-RTKmaster"
+docker build --tag ${REPO}/${NAME}:${CC}-${DCMIRTK} docker/${CC}-${DCMIRTK} 
+docker push ${REPO}/${NAME}:${CC}-${DCMIRTK} 
 
 # docker run -it --rm ${REPO}/${NAME}:gcc7-x86_64 bash
