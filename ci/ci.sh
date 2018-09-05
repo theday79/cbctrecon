2fdf22083153c62251495ce485acb2fc0d80dd75
agravgaard <andreasga22@gmail.com>
2018-09-05 13:57:44 +0200

    GitLabCI: New docker image for testing USE_SYSTEM_*, AppVeyor: Change CMake to be sure RTK libs are available to the linker

--- ci/ci.sh
+++ ci/ci.sh
@@ -12,12 +12,12 @@ mkdir build && cd build
 echo Test building: $BUILD_TESTING
 
 # TinyRefl is for static reflection in the test, HDF5 is just to reduce complilation time:
-if [ "$BUILD_TESTING" == "YES" ]; then
-    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON -DITK_USE_SYSTEM_HDF5=ON
-    cmake ..
-    make $MAKE_CONCURRENCY CbctRecon CbctRecon_test
+if [ -d /home/user/ITK-build ]; then # Use system DCMTK, ITK and RTK
+    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON -DUSE_SYSTEM_DCMTK=ON -DUSE_SYSTEM_ITK=ON -DDCMTK_DIR=/home/user/DCMTK-build -DITK_DIR=/home/uesr/ITK-build
 else
     cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=OFF -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON -DITK_USE_SYSTEM_HDF5=ON
-    cmake ..
-    make $MAKE_CONCURRENCY CbctRecon CbctRecon_test
 fi
+
+cmake ..
+make $MAKE_CONCURRENCY CbctRecon CbctRecon_test
+ctest -V
--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -6,4 +6,7 @@ NAME="cbctrecon"
 docker build --tag ${REPO}/${NAME}:gcc7-x86_64 docker/gcc-7-x86_64
 docker push ${REPO}/${NAME}:gcc7-x86_64
 
+docker build --tag ${REPO}/${NAME}:gcc7-x86_64-DCMTK3.6.3-ITKv4.13.1-RTKmaster docker/gcc-7-x86_64-DCMTK3.6.3-ITKv4.13.1-RTKmaster 
+docker push ${REPO}/${NAME}:gcc7-x86_64-DCMTK3.6.3-ITKv4.13.1-RTKmaster 
+
 # docker run -it --rm ${REPO}/${NAME}:gcc7-x86_64 bash
