c4ac184c880c97a6c4325f8c8ba604825df602b3
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2018-09-11 12:26:19 +0200

    UI improvements

--- Applications/CbctRecon/main.cpp
+++ Applications/CbctRecon/main.cpp
@@ -2,6 +2,23 @@
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
+  // Load color theme if it exists:
+  QString app_path = QCoreApplication::applicationDirPath();
+  QFile f(app_path + "/ui/style.qss");
+  if (!f.exists())
+  {
+      std::cout << "Unable to set stylesheet, file: "
+                << f.fileName().toStdString()
+                << " not found\n"
+                << std::endl;
+  }
+  else
+  {
+      f.open(QFile::ReadOnly | QFile::Text);
+      QTextStream ts(&f);
+      qApp->setStyleSheet(ts.readAll());
+  }
+
   CbctRecon w;
   w.show();
   return a.exec();
