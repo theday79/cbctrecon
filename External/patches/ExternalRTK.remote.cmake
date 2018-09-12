5628ec7b8fc79ba432d5ee7d700d94bec05e0158
agravgaard <andreasga22@gmail.com>
2018-09-12 22:28:28 +0200

    Use pre-NO_INSTALL-removal commit of RTK

--- External/patches/ExternalRTK.remote.cmake
+++ External/patches/ExternalRTK.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(RTK
   "RTK external module for ITK"
   GIT_REPOSITORY "${git_protocol}://github.com/SimonRit/RTK.git"
-  GIT_TAG master # "6becead1161dcac0f2ba59a1238168b33bb97e3f"
+  GIT_TAG 0a2d4d573ab7bcf028c1f63d1dd417db29aa8a04 # master # "6becead1161dcac0f2ba59a1238168b33bb97e3f"
 )
