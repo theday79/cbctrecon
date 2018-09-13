9d6c98ab904a40e62590382d7657791952e0092c
agravgaard <andreasga22@gmail.com>
2018-09-13 09:54:27 +0200

    Revert to building DCMTK seperately (for now), and add clang to CI

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -11,4 +11,11 @@ DCMIRTK="DCMTK363-ITKv4131-RTKmaster"
 docker build --tag ${REPO}/${NAME}:${CC}-${DCMIRTK} docker/${CC}-${DCMIRTK} 
 docker push ${REPO}/${NAME}:${CC}-${DCMIRTK} 
 
+CC="clang-6-x86_64"
+docker build --tag ${REPO}/${NAME}:${CC} docker/${CC}
+docker push ${REPO}/${NAME}:${CC}
+
+docker build --tag ${REPO}/${NAME}:${CC}-${DCMIRTK} docker/${CC}-${DCMIRTK} 
+docker push ${REPO}/${NAME}:${CC}-${DCMIRTK} 
+
 # docker run -it --rm ${REPO}/${NAME}:gcc7-x86_64 bash
