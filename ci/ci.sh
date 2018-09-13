0578e412b1f5d49821efc7e5e6ca4bccff591776
agravgaard <andreasga22@gmail.com>
2018-09-13 11:07:44 +0200

    Give ITK and DCMTK source dir as cmake variable

--- ci/ci.sh
+++ ci/ci.sh
@@ -13,7 +13,7 @@ echo Test building: $BUILD_TESTING
 
 # TinyRefl is for static reflection in the test, HDF5 is just to reduce complilation time:
 if [ -d /home/user/ITK-build ]; then # Use system DCMTK, ITK and RTK
-    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON -DUSE_SYSTEM_DCMTK=ON -DUSE_SYSTEM_ITK=ON -DDCMTK_DIR=/home/user/DCMTK-build -DITK_DIR=/home/uesr/ITK-build
+    cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=ON -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON -DUSE_SYSTEM_DCMTK=ON -DUSE_SYSTEM_ITK=ON -DDCMTK_DIR=/home/user/DCMTK-build -DDCMTK_SRC=/home/user/DCMTK -DITK_DIR=/home/user/ITK-build -DITK_SRC=/home/user/ITK
 else
     cmake .. -DUSE_CUDA=OFF -DCMAKE_INSTALL_PREFIX="/home/user/" -DBUILD_TESTING=OFF -DUSE_TINYREFL=ON -DCBCTRECON_BUILD_TESTS=ON -DITK_USE_SYSTEM_HDF5=ON
 fi
