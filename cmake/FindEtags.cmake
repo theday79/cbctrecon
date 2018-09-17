d4c9469eecf3ceb94268a256200eba77f815c1ce
agravgaard <andreasga22@gmail.com>
2018-09-17 17:05:09 +0200

    CMake improvements and new plm.patch

--- cmake/FindEtags.cmake
+++
@@ -1,7 +0,0 @@
-## From Jan Woetzel, CMAKE email list
-## http://www.cmake.org/pipermail/cmake/2006-January/007883.html
-IF (UNIX)
-  ADD_CUSTOM_TARGET(tags etags --members --declarations  `find ${CMAKE_SOURCE_DIR} -name *.cc -or -name *.cxx -or -name *.hxx -or -name *.hh -or -name *.cpp -or -name *.h -or -name *.c -or -name *.f`)
-  ADD_CUSTOM_TARGET(etags DEPENDS tags)
-  MESSAGE (STATUS "Etags targets added.")
-ENDIF (UNIX)
