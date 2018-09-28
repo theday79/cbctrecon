fe912f6854dd0095a3f32564c4ea38636a6a0d45
agravgaard <andreasga22@gmail.com>
2018-09-28 10:29:04 +0200

    Avoid using the same name for data-targets

--- cmake/ctest_setup.cmake
+++ cmake/ctest_setup.cmake
@@ -5,11 +5,14 @@ function(add_cbctrecon_test NAME)
 
     if(MEMORYCHECK_COMMAND)
         message(STATUS "Running CTest ${NAME} with valgrind")
-        ExternalData_add_test(NAME ${NAME} COMMAND ${MEMORYCHECK_COMMAND} --leak-check=full $<TARGET_FILE:${NAME}> ${ARGN})
+	ExternalData_add_test(${NAME}_Data
+            NAME ${NAME}
+	    COMMAND ${MEMORYCHECK_COMMAND} --leak-check=full $<TARGET_FILE:${NAME}> ${ARGN})
     else()
-        ExternalData_add_test(${NAME} ${NAME} ${ARGN} COMMAND $<TARGET_FILE:${NAME}> ${ARGN})
+	    ExternalData_add_test(CbctReconData
+                NAME ${NAME}
+		COMMAND $<TARGET_FILE:${NAME}> ${ARGN})
     endif()
-    ExternalData_Add_Target(${NAME})
 endfunction()
 
 enable_testing()
