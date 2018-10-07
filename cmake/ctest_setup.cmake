56341bfb68d612346a5383b3a56a5708e1751ae3
agravgaard <andreasga22@gmail.com>
2018-10-07 14:56:13 +0200

    Fix externalData download

--- cmake/ctest_setup.cmake
+++ cmake/ctest_setup.cmake
@@ -1,6 +1,31 @@
 include(CTest)
+
+##### EXTERNAL DATA #####
 get_filename_component(_CBCTRECONExternalData_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(ExternalData)
+
+if(NOT ExternalData_OBJECT_STORES)
+  # Use ExternalData_OBJECT_STORES from environment as default.
+  set(ExternalData_OBJECT_STORES_DEFAULT "")
+  if(DEFINED "ENV{ExternalData_OBJECT_STORES}")
+    file(TO_CMAKE_PATH "$ENV{ExternalData_OBJECT_STORES}" ExternalData_OBJECT_STORES_DEFAULT)
+  endif()
+endif()
+
+set(ExternalData_OBJECT_STORES "${ExternalData_OBJECT_STORES_DEFAULT}" CACHE STRING
+  "Semicolon-separated list of local directories holding data objects in the layout %(algo)/%(hash).")
+mark_as_advanced(ExternalData_OBJECT_STORES)
+if(NOT ExternalData_OBJECT_STORES)
+  set(ExternalData_OBJECT_STORES "${CMAKE_BINARY_DIR}/ExternalData/Objects")
+  file(MAKE_DIRECTORY "${ExternalData_OBJECT_STORES}")
+endif()
+list(APPEND ExternalData_OBJECT_STORES
+  # Local data store populated by the ITK pre-commit hook
+  "${CMAKE_SOURCE_DIR}/.ExternalData"
+  )
+
+set(ExternalData_BINARY_ROOT ${CMAKE_BINARY_DIR}/ExternalData)
+
 set(ExternalData_URL_ALGO_MD5_lower md5)
 set(ExternalData_URL_TEMPLATES
   # Data published on Girder
@@ -8,6 +33,8 @@ set(ExternalData_URL_TEMPLATES
   )
 
 set(ExternalData_LINK_CONTENT MD5)
+##### EXTERNAL DATA END #####
+
 
 function(add_cbctrecon_test)
   cmake_parse_arguments(
