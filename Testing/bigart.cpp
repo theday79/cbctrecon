77d070f62d4a9b26b43abd2bfc1b6908b50315c3
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2019-03-01 11:44:19 +0100

    Travis: Try gcc-8. + Make example easier scriptable from python

--- Testing/bigart.cpp
+++ Testing/bigart.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <memory>
 #include <numeric>
+#include <string>
 
 #include <QDir>
 
@@ -20,12 +21,12 @@
 
 constexpr auto deg2rad(double deg) { return deg / 180.0 * itk::Math::pi; }
 
-UShortImageType::Pointer
-get_image_from_dicom(const QString &dir,
-                     const UShortImageType::SpacingType &new_spacing,
-                     const UShortImageType::SizeType &new_size,
-                     const UShortImageType::PointType &new_origin,
-                     const UShortImageType::DirectionType &new_direction) {
+UShortImageType::Pointer get_image_from_dicom(
+    const QString &dir, const UShortImageType::SpacingType &new_spacing,
+    const UShortImageType::SizeType &new_size,
+    const UShortImageType::PointType &new_origin,
+    const UShortImageType::DirectionType &new_direction,
+    const DoubleVector &translation_vec, const DoubleVector &rotation_vec) {
   auto cbctrecon_test = std::make_unique<CbctReconTest>();
 
   auto dcm_dir = QDir(dir);
@@ -64,12 +65,18 @@ get_image_from_dicom(const QString &dir,
       itk::ResampleImageFilter<UShortImageType, UShortImageType>::New();
 
   auto transform = itk::Euler3DTransform<double>::New();
-  transform->SetRotation(deg2rad(-0.8843), deg2rad(1.6274), deg2rad(-0.3450));
+  transform->SetRotation(deg2rad(-rotation_vec.x), deg2rad(-rotation_vec.y),
+                         deg2rad(-rotation_vec.z));
+  // transform->SetRotation(deg2rad(-0.8843), deg2rad(1.6274),
+  // deg2rad(-0.3450));
 
   auto translation = itk::Euler3DTransform<double>::InputVectorType();
-  translation.SetElement(0, 5.5185);  // -X in eclipse -> X itk
-  translation.SetElement(1, -2.6872); // -Y in eclipse -> Y itk
-  translation.SetElement(2, -6.4281); // -Z in eclipse -> Z itk
+  translation.SetElement(
+      0, -translation_vec.x); // 5.5185);  // -X in eclipse -> X itk
+  translation.SetElement(
+      1, -translation_vec.y); // -2.6872); // -Y in eclipse -> Y itk
+  translation.SetElement(
+      2, -translation_vec.z); // -6.4281); // -Z in eclipse -> Z itk
   transform->SetTranslation(translation);
 
   resampler->SetInput(ct_img);
@@ -88,13 +95,20 @@ get_image_from_dicom(const QString &dir,
 
 int main(const int argc, char *argv[]) {
 
-  if (argc < 3) {
+  if (argc < 6) {
     std::cerr << "Usage:\n"
-              << argv[0] << " /plan/dicom/dir /recalc/dicom/dir\n";
+              << argv[0]
+              << " /plan/dicom/dir /recalc/dicom/dir voi_name gantry couch "
+                 "translation rotation\n"
+              << "Where translation[mm] and rotation[deg] is comma-seperated "
+                 "triples in the eclipse coordinate system\n";
     return -1;
   }
 
-  std::cerr << "Running BiGART script!\n";
+  std::cerr << "Running BiGART script!\n"
+            << " " << argv[0] << " " << argv[1] << " " << argv[2] << " "
+            << argv[3] << " " << argv[4] << " " << argv[5] << " " << argv[6]
+            << " " << argv[7] << "\n";
 
   /* Read dicom and structures */
   auto cbctrecon_test = std::make_unique<CbctReconTest>();
@@ -119,12 +133,24 @@ int main(const int argc, char *argv[]) {
     std::cerr << "Manual Rigid CT was NULL -> Dicom dir was not read!\n";
     return -4;
   }
+  std::string::size_type sz;
+  auto translation_str = QString(argv[6]);
+  auto translation = DoubleVector{
+      std::stod(translation_str.split(",").at(0).toStdString(), &sz),
+      std::stod(translation_str.split(",").at(1).toStdString(), &sz),
+      std::stod(translation_str.split(",").at(2).toStdString(), &sz)};
+  auto rotation_str = QString(argv[7]);
+  auto rotation =
+      DoubleVector{std::stod(rotation_str.split(",").at(0).toStdString(), &sz),
+                   std::stod(rotation_str.split(",").at(1).toStdString(), &sz),
+                   std::stod(rotation_str.split(",").at(2).toStdString(), &sz)};
 
   auto recalc_dcm_dir = QString(argv[2]);
-  auto recalc_img =
-      get_image_from_dicom(recalc_dcm_dir, ct_img->GetSpacing(),
-                           ct_img->GetLargestPossibleRegion().GetSize(),
-                           ct_img->GetOrigin(), ct_img->GetDirection());
+
+  auto recalc_img = get_image_from_dicom(
+      recalc_dcm_dir, ct_img->GetSpacing(),
+      ct_img->GetLargestPossibleRegion().GetSize(), ct_img->GetOrigin(),
+      ct_img->GetDirection(), translation, rotation);
 
   saveImageAsMHA<UShortImageType>(recalc_img,
                                   recalc_dcm_dir.toStdString() + "/recalc.mha");
@@ -136,9 +162,13 @@ int main(const int argc, char *argv[]) {
   auto voi = std::string();
 
   for (auto &structure : ss->slist) {
-    if (structure.name.compare("GTV 74/37 LB") == 0) {
+    if (structure.name.compare(argv[3]) == 0) { // "GTV 74/37 LB") == 0) {
       voi = structure.name;
     }
+  }
+  if (voi.empty()) {
+    std::cerr << "Couldn't find: \"" << argv[3] << "\"\n";
+    return -5;
   }
   std::cerr << voi << "\n";
 
@@ -159,8 +189,8 @@ int main(const int argc, char *argv[]) {
             << ", " << last_point[2] << "\n";
 
   auto &orig_voi = ss->get_roi_ref_by_name(voi);
-  const auto gantry_angle = 90;
-  const auto couch_angle = 0;
+  const auto gantry_angle = static_cast<int>(round(std::stod(argv[4], &sz)));
+  const auto couch_angle = static_cast<int>(round(std::stod(argv[5], &sz)));
   cbctrecon_test->m_cbctregistration->CalculateWEPLtoVOI(
       voi, gantry_angle, couch_angle, ct_img, recalc_img);
 
@@ -187,24 +217,32 @@ int main(const int argc, char *argv[]) {
       });
 
   /* Write distances to file */
+  auto better_name = orig_voi.name;
+  std::replace(std::begin(better_name), std::end(better_name), ' ', '_');
+  std::replace(std::begin(better_name), std::end(better_name), '/', '-');
   auto output_filename =
-      "Dist_" + orig_voi.name + "_CT" + dcm_path.back().toLatin1() + "_to_CT" +
-      recalc_dcm_dir.back().toLatin1() + "_at_G" +
+      "Dist_" + better_name + "_CT" +
+      dcm_path.at(dcm_path.size() - 1).toLatin1() + "_to_CT" +
+      recalc_dcm_dir.at(recalc_dcm_dir.size() - 1).toLatin1() + "_at_G" +
       std::to_string(gantry_angle) + "C" + std::to_string(couch_angle) + ".txt";
 
   std::ofstream f_stream;
   f_stream.open(output_filename);
-  f_stream << std::fixed << std::setprecision(5);
+  if (f_stream.is_open()) {
+    f_stream << std::fixed << std::setprecision(5);
 
-  std::for_each(std::begin(output), std::end(output),
-                [&f_stream](const std::vector<float> out_vec) {
-                  std::for_each(std::begin(out_vec), std::end(out_vec),
-                                [&f_stream](const float val) {
-                                  f_stream << val << "\n";
-                                });
-                });
+    std::for_each(std::begin(output), std::end(output),
+                  [&f_stream](const std::vector<float> out_vec) {
+                    std::for_each(std::begin(out_vec), std::end(out_vec),
+                                  [&f_stream](const float val) {
+                                    f_stream << val << "\n";
+                                  });
+                  });
 
-  f_stream.close();
+    f_stream.close();
+  } else {
+    std::cerr << "Could not open file: " << output_filename << "\n";
+  }
 
   return 0;
 }
