a207bbae650cf8778f3acc06f567bb70d14308e2
agravgaard <andreasga22@gmail.com>
2019-04-16 18:04:15 +0200

    GitLab-CI: Try printing a summary of coverage

--- ci/ci.sh
+++ ci/ci.sh
@@ -35,5 +35,6 @@ ctest -VV
 
 if [[ "$COVERAGE" = "YES" ]]; then
   lcov --directory Testing --base-directory ../Library/CbctReconLib/ --capture --output-file coverage.info
-  cat coverage.info
+  lcov --list coverage.info
+  lcov --summary coverage.info
 fi
