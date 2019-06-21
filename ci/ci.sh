38d8a428e4ec590131311dad7f7ff6aea9a21a44
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2019-06-21 13:18:30 +0200

    [CI] Clean up + Fix CUDA runtime lib path

--- ci/ci.sh
+++ ci/ci.sh
@@ -13,34 +13,25 @@ export COMMON_FLAGS=".. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=
 export COMMON_SYSTEM_LIBS="-DUSE_SYSTEM_ZLIB=ON -DUSE_SYSTEM_DCMTK=ON -DHUNTER_ENABLED=OFF"
 
 if [ -d /home/user/ITK-build ]; then # Use system ITK and RTK
-        cmake $COMMON_FLAGS \
-            -DUSE_CUDA=OFF -DCBCTRECON_COVERAGE=OFF \
-            -DCBCTRECON_BUILD_TESTS=ON -DUSE_SYSTEM_ITK=ON \
-            -DITK_DIR=/home/user/ITK-build \
-            $COMMON_SYSTEM_LIBS
+    export COMMON_NONSYSTEM_ITK="-DUSE_SYSTEM_ITK=ON -DITK_DIR=/home/user/ITK-build"
 else
     # USE_SYSTEM_EIGEN = OFF with hunter disabled actually triggers the use of system eigen (will be corrected eventually)
     export COMMON_NONSYSTEM_ITK="-DITK_USE_SYSTEM_DCMTK=ON -DUSE_DCMTK=ON -DUSE_ITK_DCMTK=OFF -DUSE_SYSTEM_EIGEN=OFF"
-    if [[ "$CUDA_AVAILABLE" = "YES" ]]; then
-        if [[ "$COVERAGE" = "YES" ]]; then
-            cmake $COMMON_FLAGS \
-                -DUSE_CUDA=ON -DEXACT_GCC="/usr/bin/gcc-7" \
-                -DCBCTRECON_COVERAGE=ON \
-                $COMMON_SYSTEM_LIBS $COMMON_NONSYSTEM_ITK
-        else
-            cmake $COMMON_FLAGS
-                -DUSE_CUDA=ON -DEXACT_GCC="/usr/bin/gcc-7" \
-                -DCBCTRECON_COVERAGE=OFF \
-                $COMMON_SYSTEM_LIBS $COMMON_NONSYSTEM_ITK
-        fi
-    else
-        cmake $COMMON_FLAGS \
-            -DUSE_CUDA=OFF -DCBCTRECON_COVERAGE=OFF \
-            $COMMON_SYSTEM_LIBS $COMMON_NONSYSTEM_ITK
-    fi
 fi
 
-cmake ..
+if [[ "$CUDA_AVAILABLE" = "YES" ]]; then
+    export CUDA_FLAGS="-DUSE_CUDA=ON -DEXACT_GCC=/usr/bin/gcc-7"
+else
+    export CUDA_FLAGS="-DUSE_CUDA=OFF"
+fi
+
+if [[ "$COVERAGE" = "YES" ]]; then
+    export COVERAGE_FLAGS="-DCBCTRECON_COVERAGE=ON"
+else
+    export COVERAGE_FLAGS="-DCBCTRECON_COVERAGE=OFF"
+fi
+
+cmake $COMMON_FLAGS $COMMON_SYSTEM_LIBS $COMMON_NONSYSTEM_ITK $CUDA_FLAGS $COVERAGE_FLAGS
 cmake --build . --target CbctData
 cmake --build .
 
