210550e8f12ee0cc8b8ccce201cea8e2ceb62530
Andreas Gravgaard Andersen <andreasga22@gmail.com>
2020-08-21 15:06:26 +0200

    [External] Use fork of plastimatch instead of massive unreliable patch

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -1,11 +1,11 @@
 include(${CbctRecon_SOURCE_DIR}/cmake/DownloadProject.cmake)
 
 macro(external_dependency NAME URL COMMIT)
-  if(${NAME} STREQUAL "Plastimatch")
-    set(PATCH_CMD PATCH_COMMAND git apply ${CMAKE_SOURCE_DIR}/External/patches/plm.patch)
-  else()
-    set(PATCH_CMD PATCH_COMMAND "")
-  endif()
+  # if(${NAME} STREQUAL "Plastimatch")
+  #   set(PATCH_CMD PATCH_COMMAND "") git apply ${CMAKE_SOURCE_DIR}/External/patches/plm.patch)
+  # else()
+  set(PATCH_CMD PATCH_COMMAND "")
+
   if(${NAME} STREQUAL "TINYREFL")
     set(SHALLOW_CMD GIT_SHALLOW OFF)
   else()
